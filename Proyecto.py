2c6c2fe83984ee49f25e94eebec76aa4ac85f869
Sch17232 <45051208+Sch17232@users.noreply.github.com>
2018-11-15 18:27:00 -0600

    Version 2
    Ya sirve bien el cambio de loops

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-#Proyecto final Rony Schumann y Luis Fernando de leÃ³n
+#Proyecto final Rony Schumann y Luis Fernando de león
 
 #importar librerias
 from Tkinter import *
@@ -8,27 +8,97 @@ import time
 import sys
 #from PIL import Image, ImageTk
 
-lista1 = []
-lista2 = []
-lista3 = []
-
+Grabar_ = 0
+Playstation = 0
 Manual = 1
-Pleito = 0
-Grabar = 0
-Rut1 = 1
-Rut2 = 0
-Rut3 = 0
-
+Rutina_1 = 0
+Rutina_2 = 0
 #lista1.append()
 
-def cambio(var_1, var_2, var_3, var_4, var_5, var_6):
-    var_1 = 1
-    var_2 = 1
-    var_3 = 0
-    var_4 = 0
-    var_5 = 0
-    var_6 = 0
+def Stop_():
+    global Grabar_
+    global Playstation
+    global Manual
+    global Rutina_1
+    global Rutina_2
+    Grabar_ = 0
+    Playstation = 0
+    Manual = 1
+    Rutina_1 = 0
+    Rutina_2 = 0
+
+def play_1():
+    global Grabar_
+    global Playstation
+    global Manual
+    global Rutina_1
+    global Rutina_2
+    Grabar_ = 0
+    Playstation = 1
+    Manual = 0
+    Rutina_1 = 1
+    Rutina_2 = 0
+
+def play_2():
+    global Grabar_
+    global Playstation
+    global Manual
+    global Rutina_1
+    global Rutina_2
+    Grabar_ = 0
+    Playstation = 1
+    Manual = 0
+    Rutina_1 = 0
+    Rutina_2 = 1
+
+def play_3():
+    global Grabar_
+    global Playstation
+    global Manual
+    global Rutina_1
+    global Rutina_2
+    Grabar_ = 0
+    Playstation = 1
+    Manual = 0
+    Rutina_1 = 0
+    Rutina_2 = 0
     
+def rec_1():
+    global Grabar_
+    global Playstation
+    global Manual
+    global Rutina_1
+    global Rutina_2
+    Grabar_ = 1
+    Playstation = 0
+    Manual = 0
+    Rutina_1 = 1
+    Rutina_2 = 0
+
+def rec_2():
+    global Grabar_
+    global Playstation
+    global Manual
+    global Rutina_1
+    global Rutina_2
+    Grabar_ = 1
+    Playstation = 0
+    Manual = 0
+    Rutina_1 = 0
+    Rutina_2 = 1
+
+def rec_3():
+    global Grabar_
+    global Playstation
+    global Manual
+    global Rutina_1
+    global Rutina_2
+    Grabar_ = 1
+    Playstation = 0
+    Manual = 0
+    Rutina_1 = 0
+    Rutina_2 = 0
+
 
 #VENTANA
 ventana = Tk()
@@ -38,68 +108,56 @@ ventana.configure(bg = '#CED8F6')
 #NOMBRES
 Nombre = Label(ventana, text = "Rony Schumann 17232", bg = '#CED8F6', font=("Times", 10))
 Nombre.grid(row=0, column=0)
-Nombre2 = Label(ventana, text = "Tusitar de LeÃ³n 17124", bg = '#CED8F6', font=("Times", 10))
+Nombre2 = Label(ventana, text = "Tusitar de León 17124", bg = '#CED8F6', font=("Times", 10))
 Nombre2.grid(row=1, column=0,pady=(1, 50))
 
 #FILA1
 texto2 = Label(ventana, text = "Rutina 1", bg = '#CED8F6',font=("Times", 14, "bold"))
 texto2.grid(row=2, column=0,padx=(75, 10))
 #BOTONES 1
-boton2 = Button(ventana, text = 'Grabar', command = "funcion", bd = 10)
+boton2 = Button(ventana, text = 'Grabar', command = rec_1, bd = 10)
 boton2.grid(row=2, column=1)
-boton2_2 = Button(ventana, command = "funcion", text = "Reproducir", bd = 10)
+boton2_2 = Button(ventana, command = play_1, text = 'Reproducir', bd = 10)
 boton2_2.grid(row=2, column=2,padx=10, pady=10)
-boton2_3 = Button(ventana, text="Parar", command = cambio(Manual, Rut1, Pleito, Grabar, Rut2, Rut3), bd = 10)
+boton2_3 = Button(ventana, text="Parar", command = Stop_, bd = 10)
 boton2_3.grid(row=2, column=3)
 
 #FILA2
 texto3 = Label(ventana, text = "Rutina 2", bg = '#CED8F6', font=("Times", 14, "bold"))
 texto3.grid(row=3, column=0,padx=(75, 10))
 #BOTONES 2
-boton3 = Button(ventana, text="Grabar", command = "funcion", bd = 10)
+boton3 = Button(ventana, text="Grabar", command = rec_2, bd = 10)
 boton3.grid(row=3, column=1)
-boton3_2 = Button(ventana, text="Reproducir", command = "funcion", bd = 10)
+boton3_2 = Button(ventana, text="Reproducir", command = play_2, bd = 10)
 boton3_2.grid(row=3, column=2,padx=10, pady=10)
-boton3_3 = Button(ventana, text="Parar", command = cambio(Manual, Rut2, Pleito, Grabar, Rut1, Rut3), bd = 10)
+boton3_3 = Button(ventana, text="Parar", command = Stop_ , bd = 10)
 boton3_3.grid(row=3, column=3)
 
 #FILA3
 texto1 = Label(ventana, text = "Rutina 3", bg = '#CED8F6', font=("Times", 14, "bold"))
 texto1.grid(row=4, column=0,padx=(75, 10))
 #BOTONES 3
-boton1 = Button(ventana, text="Grabar", command = "funcion", bd = 10)
+boton1 = Button(ventana, text="Grabar", command = rec_3, bd = 10)
 boton1.grid(row=4, column=1)
-boton1_2 = Button(ventana, text="Reproducir", command = "funcion", bd = 10)
+boton1_2 = Button(ventana, text="Reproducir", command = play_3, bd = 10)
 boton1_2.grid(row=4, column=2,padx=10, pady=10)
-boton1_3 = Button(ventana, text="Parar", command = cambio(Manual, Rut3, Pleito, Grabar, Rut2, Rut1), bd = 10)
+boton1_3 = Button(ventana, text="Parar", command = Stop_, bd = 10)
 boton1_3.grid(row=4, column=3)
 
-while 1:
-    time.sleep
-    print Manual
-    print Pleito
-    print Grabar
-    print Rut1
-    print Rut2
-    print Rut3
-    ventana.update()
 
-###LOOPS
-##ser= serial.Serial(port='COM5',baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS, timeout=0)
-##
-##while Manual == 1:
-##   print 'hola'
-##   ventana.update()
-##    
-##while Grabar == 1:
-##    print 'adios'
-##    ventana.update()
-##    
-##while Pleito == 1:
-##    print 'adios'
-##    ventana.update()
-##
-##
-##
-##
-##
+#LOOPS
+#ser= serial.Serial(port='COM5',baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS, timeout=0)
+while 1:
+    while Manual == 1:
+       print 'dele_duro'
+       ventana.update()
+        
+    while Grabar_ == 1:
+        
+        print 'grabe'
+        ventana.update()
+        
+    while Playstation == 1:
+        print 'play'
+        ventana.update()
+
